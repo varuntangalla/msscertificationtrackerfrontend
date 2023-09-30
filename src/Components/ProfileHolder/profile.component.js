354ccd16ceb8eae9324670bd218557c40fe2e780
Satish-Kumar-Kun <63464652+Satish-Kumar-Kun@users.noreply.github.com>
2023-09-30 13:15:06 -0400

    Updated Employee Not Found Page

--- src/Components/ProfileHolder/profile.component.js
+++ src/Components/ProfileHolder/profile.component.js
@@ -1,6 +1,8 @@
 //import ProfileInfo from "../ProfileInfo/profileInfo.component";
 import ProfileCertificationTimeline from "../ProfileCertificateTimeline/profileCertificateTimeline.component";
 import { useParams } from "react-router-dom";
+import { Typography, Box } from "@mui/material";
+import WarningIcon from "@mui/icons-material/Warning";
 function ProfileComponent({ data }) {
   const { employeeId } = useParams();
   const employee = data.find(
@@ -9,7 +11,14 @@ function ProfileComponent({ data }) {
 
   // If employee is not found, you can render an error message or redirect
   if (!employee) {
-    return <div>Employee not found</div>;
+    return (
+      <Box display="flex" flexDirection="column" alignItems="center" mt={4}>
+        <Typography variant="h6" color="error">
+          Employee Details Not Found
+        </Typography>
+        <WarningIcon color="error" sx={{ fontSize: 40, marginTop: 2 }} />
+      </Box>
+    );
   }
   return (
     <div>
