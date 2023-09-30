92b1503fe6cd9b5a8ff5b3dd00b47f19c7076b7a
Saivarun Tangalla <saivaruntangalla@Saivaruns-MacBook-Pro.local>
2023-09-30 14:58:26 -0400

    testing ci/cd

--- src/Components/PageNotFound/pageNotFound.component.js
+++ src/Components/PageNotFound/pageNotFound.component.js
@@ -7,7 +7,7 @@ const PageNotFound = () => {
   return (
     <Box display="flex" flexDirection="column" alignItems="center" mt={4}>
       <Typography variant="h2" color="textPrimary">
-        Page Not Found
+        Page Not Found 404
       </Typography>
       <ErrorIcon color="error" style={{ fontSize: 100, marginTop: 10 }} />
     </Box>
