289dd00cad2e26a7a474c61e7a04a242b03dfbb9
Satish-Kumar-Kun <63464652+Satish-Kumar-Kun@users.noreply.github.com>
2023-10-01 20:09:06 -0400

    Fixed async data bug

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import "./App.css";
 import React, { useState, useEffect } from "react";
 import ProfileComponent from "./Components/ProfileHolder/profile.component";
 import MyNavbar from "./Components/Navbar/navbar.component";
-import Footer from "./Components/Footer/footer.component"; // Import the Footer component
-// import PageNotFound from "./Components/PageNotFound/pageNotFound.component"; // Import the PageNotFound component
+import Footer from "./Components/Footer/footer.component";
+import PageNotFound from "./Components/PageNotFound/pageNotFound.component";
 import { Route, Routes } from "react-router-dom";
-import db from "./Services/firebaseConfig.js"; // Path to your firebaseConfig.js
+import db from "./Services/firebaseConfig.js";
 import { collection, getDocs } from "firebase/firestore";
 
 function App() {
   const [items, setItems] = useState([]);
-  // useEffect(() => {
-  //   setItems(require("./assets/employeeList.json"));
-  // }, []);
+  const [dataLoaded, setDataLoaded] = useState(false);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -24,6 +22,7 @@ function App() {
           ...doc.data(),
         }));
         setItems(dataArray);
+        setDataLoaded(true);
       } catch (error) {
         console.error("Error fetching data: ", error);
       }
@@ -31,43 +30,21 @@ function App() {
 
     fetchData();
   }, []);
-  console.log("DataArray", items);
-  // useEffect(() => {
-  //   fetch("/assets/employeeList.json")
-  //     .then((response) => {
-  //       if (!response.ok) {
-  //         throw new Error(`HTTP error! Status: ${response.status}`);
-  //       }
-  //       return response.json();
-  //     })
-  //     .then((data) => {
-  //       setItems(data);
-  //     })
-  //     .catch((error) => {
-  //       console.error("Error fetching data:", error);
-  //     });
-  // }, []);
 
-  // if (items.length === 0) {
-  //   return <div>Loading...</div>;
-  // }
+  if (!dataLoaded) {
+    return <div></div>;
+  }
 
   return (
     <div>
       <MyNavbar />
-      {/* Render the dynamic card component with the specific employeeId from the URL */}
       <Routes>
-        {/* Route to handle dynamic employeeId from the URL */}
         <Route
           path="/employee/:employeeId"
-          // element={<ActionAreaCard data={items} />
           element={<ProfileComponent data={items} />}
         />
-        {/* <Route path="/*" element={<PageNotFound data={"Page Not Found!"} />} /> */}
+        <Route path="/*" element={<PageNotFound data={"Page Not Found!"} />} />
       </Routes>
-      {/* <ActionAreaCard data={items} match={{ params: { employeeId: "1221" } }} /> */}
-      {/* Replace "1220" with the actual employeeId from the URL */}
-      {/* Redirect to EmployeeNotFound if the path doesn't match */}
       <Footer />
     </div>
   );
